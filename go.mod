1a1b38eb916877aea464272b39e3af92f7f3cd98
Daniel Aaron <danjaaron@gmail.com>
2021-01-23 04:17:18 -0500

    trying a different nmap library

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/Ullaakut/nmap/v2 v2.0.3 // indirect
 	github.com/cheggaaa/pb/v3 v3.0.5 // indirect
+	github.com/dean2021/go-nmap v0.0.0-20181206040457-deecb3c7af98 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 )
--- scan-random-subnets.go
+++ scan-random-subnets.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "context"
     "fmt"
     "log"
     "time"
@@ -9,7 +8,8 @@ import (
     "math/rand"
     "strconv"
 
-    "github.com/Ullaakut/nmap/v2"
+    //"github.com/Ullaakut/nmap/v2"
+    "github.com/dean2021/go-nmap"
     "github.com/cheggaaa/pb/v3"
 )
 
@@ -19,8 +19,6 @@ func makeRandomIP() string {
 }
 
 func main() {
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-    defer cancel()
 
     // set random number seed
     rand.Seed(time.Now().Unix())
@@ -51,6 +49,35 @@ func main() {
 	    logger.Printf(targetStr)
 
 	    // prepare scanner
+	    n := nmap.New()
+	    args := []string{"-n"}
+	    n.SetArgs(args...)
+	    n.SetPorts(port)
+	    n.SetHosts(target)
+
+	    // scan
+	    err := n.Run()
+	    bar.Increment()
+	    if err != nil {
+		    logger.Printf("scanner failed: ", err)
+	    }
+
+	    result, err := n.Parse()
+	    if err != nil {
+		    fmt.Println("Parse scanner result: ", err)
+		    continue
+	    }
+
+	    for _, host := range result.Hosts {
+		ipAddr := host.Addresses[0].Addr
+		for _, port := range host.Ports {
+			portStr := strconv.Itoa(port.PortId)
+			servicesStr := port.Service.Name
+			logger.Printf(ipAddr, portStr, servicesStr)
+		}
+	}
+	    
+	    /*
 	    scanner, err := nmap.NewScanner(
 		nmap.WithTargets(target),
 		nmap.WithPorts(os.Args[1]),
@@ -72,6 +99,7 @@ func main() {
 	    if warnings != nil {
 		logger.Printf("Warnings: \n %v", warnings)
 	    }
+	    
 
 	    // Use the results to print an example output
 	    for _, host := range result.Hosts {
@@ -87,7 +115,7 @@ func main() {
 	    }
 
 	    logger.Printf("Nmap done: %d hosts up scanned in %3f seconds\n", len(result.Hosts), result.Stats.Finished.Elapsed)
-
+	    */
     }
     bar.Finish()
 }
